8fbbb50543ec41b6a382c2bca1a114e483dbd311
Mica Linscheid <micascheid@gmail.com>
2023-10-03 12:07:51 -0600

    prod

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,9 +4,9 @@ import {doc} from "firebase/firestore";
 export const AUTO = 'auto';
 export const MANUAL = 'manual';
 //local
-export const BASE_URL = 'http://127.0.0.1:5001';
+// export const BASE_URL = 'http://127.0.0.1:5001';
 //production
-// export const BASE_URL = 'https://fluencymetrics-backend-uroarqqgxa-uc.a.run.app';
+export const BASE_URL = 'https://fluencymetrics-backend-uroarqqgxa-uc.a.run.app';
 
 export const repWholeWord = "Rep. Whole Word";
 export const repSyllable = "Rep. Syllable";
